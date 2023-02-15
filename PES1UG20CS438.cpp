ea5620d86d6f1c945d532397088f8360065755d4
such4869 <76531986+such4869@users.noreply.github.com>
2023-02-15 11:29:54 +0530

    PES1UG20CS438.cpp

---
+++ PES1UG20CS438.cpp
@@ -0,0 +1,29 @@
+pipeline{
+  agent any
+  stages{
+      stage('Build'){
+        steps{
+          sh 'g++ ./main/PES1UG20CS438.cpp'
+          build job : 'PES1UG20CS438-1'
+        }
+      }
+      stage('Test'){
+        steps{
+          sh './a.out'
+          echo 'Test Stage Successful'
+        }
+      }
+      stage('Deploy'){
+        steps{
+          echo 'Deployed'
+        }
+      }
+    }
+    post{
+      failure{
+        echo 'Pipeline failed'
+      }
+    }
+  }
+}    
+  
